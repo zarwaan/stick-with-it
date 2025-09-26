fc2f76bacd9334dad9cbf50429336e60e2b0e352
zarwaan <zarwaanshroff@gmail.com>
2025-09-27 02:51:12 +0530

    added back button and nav buttons in month view

--- src/components/CalendarView/MonthView.jsx
+++ src/components/CalendarView/MonthView.jsx
@@ -4,6 +4,8 @@ import { motion } from "motion/react";
 import Fade from "../utils/Fade";
 import dayjs from "dayjs";
 import { useHabitListContext } from "../../providers/HabitListProvider";
+import { Undo2 } from "lucide-react";
+import { useCallback } from "react";
 
 const Date = ({date, month, year, dayWiseHabits}) => {
     if(!date) return ( <div></div> )
@@ -17,12 +19,12 @@ const Date = ({date, month, year, dayWiseHabits}) => {
                     <div className="">
                         {date}
                     </div>
-                    <div className="text-md h-14 flex flex-center">
-                        <div className="h-75/100 w-9/10 overflow-hidden mb-2 text-sm text-ellipsis">
+                    <div className="text-md flex flex-center mt-2">
+                        <div className="w-9/10 overflow-hidden mb-2 text-sm text-ellipsis truncate">
                         {
-                            habits.map(h => {
+                            habits.map((h,i) => {
                                 return (
-                                    <span className="">
+                                    <span className="" key={i}>
                                         {h}
                                     </span>
                                 )
@@ -50,8 +52,27 @@ const WeekTitles = () => {
     )
 }
 
+const BackButton = ({year}) => {
+    const {setYearView} = useCalendarContext();
+    return (
+        <div className=" w-fit absolute">
+            <button className="flex flex-row h-fit gap-1 cursor-pointer hover:underline"
+                    onClick={()=>{
+                        setYearView(year)
+                    }}>
+                <div className=" h-fit flex m-auto">
+                    <Undo2 size={16}/>
+                </div>
+                <div className="text-base">
+                    {year}
+                </div>
+            </button>
+        </div>
+    )
+}
+
 export default function MonthView() {
-    const {month, year} = useCalendarContext();
+    const {month, year, setMonth, setYear} = useCalendarContext();
     const {allHabits} = useHabitListContext();
     const dayWiseHabits = {}
     week.forEach(d => dayWiseHabits[d] = [])
@@ -62,15 +83,41 @@ export default function MonthView() {
         })
     })
     // console.log(dayWiseHabits);
-    
+
+    const LeftRightButton = ({op}) => {
+        return (
+            <motion.button className="w-fit cursor-pointer" initial={{color: "rgb(13, 84, 43)"}} 
+                        whileHover={{color: "rgb(0,130,53)"}} whileTap={{y:1}}
+                        onClick={() => {
+                            if(month===1 && op===-1){
+                                setYear(y => y-1)
+                                setMonth(12)
+                            }
+                            else if(month===12 && op===1){
+                                setYear(y => y+1)
+                                setMonth(1)
+                            }
+                            else
+                                setMonth(month => month + op)
+                        }}
+                        >
+                <i className={`bi ${op===-1 ? "bi-caret-left-fill" : "bi-caret-right-fill"}`} />
+            </motion.button>
+        )
+    }
 
     const finalDaysToShow = getDaysOfMonthWithNulls(month,year);
     const areLastSevenNull = () => finalDaysToShow.slice(-7).every(e => e === null)
     //check again
     return (
         <motion.div className="h-[97%] flex flex-col gap-4">
-            <div className="text-4xl font-bold text-green-900">
-                {monthList()[month-1]}
+            <div className="text-4xl font-bold text-green-900 relative">
+                <BackButton year={year}/>
+                <div className="w-5/10 flex flex-row justify-between m-auto">
+                    <LeftRightButton op={-1} />
+                    {monthList()[month-1]}
+                    <LeftRightButton op={1} />
+                </div>
             </div>
             <div className="w-9/10 ml-auto mr-auto flex flex-1 flex-col gap-2">   
                 <WeekTitles/>         
