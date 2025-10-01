e68d27216174274b1bf1380eff1e3bf2b4c208d2
zarwaan <zarwaanshroff@gmail.com>
2025-10-01 16:59:49 +0530

    finished view for dateview for today

--- src/components/CalendarView/DateView.jsx
+++ src/components/CalendarView/DateView.jsx
@@ -1,9 +1,10 @@
 import dayjs from "dayjs";
 import { useCalendarContext } from "../../providers/CalendarProvider";
-import { Undo2 } from "lucide-react";
+import { ListChecks, StickyNote, Undo2 } from "lucide-react";
 import { monthList, weekFromSunday } from "../../helpers/calendar";
 import { useHabitListContext } from "../../providers/HabitListProvider";
 import ProgressRing from "../utils/ProgressRing";
+import InfoMessage from "../utils/InfoMessage";
 
 const isTodayBeforeOrAfter = (date, month, year) => {
     const thisDate = dayjs(`${year}-${month}-${date}`);
@@ -33,11 +34,21 @@ const BackButton = ({month}) => {
 
 const Habit = ({habit, completed=true, access=true}) => {
     return (
-        <div className="border flex flex-row p-1 gap-2 rounded-full">
-            <div className="w-1/10 border aspect-square rounded-full">
-                {habit.habit_emoji}
+        <div className={`border flex flex-row p-2 gap-2 rounded-full border-3 
+            ${
+                completed ? "border-green-900 bg-green-100" : "border-red-900 bg-red-200"
+            }
+        `}>
+            <div className={`w-1/10 border-5 aspect-square rounded-full flex flex-center text-2xl
+                ${
+                    completed ? "border-green-700" : "border-red-700"
+                }    
+            `}>
+                <span className="text-shadow-[-3px_3px_5px_rgb(0_0_0_/_0.5)]">
+                    {habit.habit_emoji}
+                </span>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 flex flex-center text-2xl px-2 font-semibold">
                 {habit.habit_title}
             </div>
         </div>
@@ -45,7 +56,7 @@ const Habit = ({habit, completed=true, access=true}) => {
 }
 
 const HabitListBox = ({name, access=true, children}) => (
-    <div className="border border-blue-500 h-5/10 flex flex-col flex flex-col gap-1">
+    <div className="border h-5/10 flex flex-col gap-1 p-2 min-h-0 border-2 border-green-900 rounded-xl">
         <div className="w-fit text-lg font-semibold text-green-900">
             {name}:
         </div>
@@ -74,25 +85,45 @@ export default function DateView() {
                 <BackButton month={month}/>
                 {dayName}, {date} {monthList()[month-1]} {year}
             </div>
-            <div className="flex flex-row gap-1 flex-1 min-h-0">
-                <div className="border flex flex-1 flex-col gap-">
+            <div className="flex flex-row gap-2 flex-1 min-h-0">
+                <div className="flex flex-1 flex-col gap-2">
                     <HabitListBox name={"Completed"}>
                         {
-                            completed.map((h,i) => (
-                                <Habit habit={h} key={i}/>
-                            ))
+                            completed.length === 0 ?
+                                <span className="text-xl">
+                                    <InfoMessage
+                                        message={"No habits completed!"}
+                                        IconToShow={StickyNote}
+                                        iconSize={33}
+                                        strokeWidth={1.5}
+                                    /> 
+                                </span>
+                            :
+                                completed.map((h,i) => (
+                                    <Habit habit={h} key={i}/>
+                                ))
                         }
                     </HabitListBox>
                     <HabitListBox name={"Remaining"}>
                     {
-                        remaining.map((h,i) => (
-                            <Habit habit={h} key={i}/>
-                        ))
+                        remaining.length === 0 ?
+                            <span className="text-xl">
+                                <InfoMessage
+                                    message={"No habits remaining!"}
+                                    IconToShow={ListChecks}
+                                    iconSize={33}
+                                    strokeWidth={1.5}
+                                /> 
+                            </span>
+                        :
+                            remaining.map((h,i) => (
+                                <Habit habit={h} key={i} completed={false}/>
+                            ))
                     }
                     </HabitListBox>
                 </div>
-                <div className="border w-3/10">
-                    <div className="aspect-square w-8/10 border m-auto mt-[10%]">
+                <div className="flex flex-center w-3/10 border rounded-xl border-green-900 border-2">
+                    <div className="aspect-square w-8/10 m-auto ">
                         <ProgressRing 
                             progress={Math.round((completed.length * 100)/(completed.length + remaining.length))} 
                             stroke={18}
